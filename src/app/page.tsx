3970086979a4aaf91ae6df80643275a1b34cc6ab
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-05-28 06:57:48 +0330

    added typography styles to tailwind and app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-red-500 text-center dark:bg-blue-500">hello arman</main>
+    <main className="bg-red-500 text-center dark:bg-blue-500">
+      <h1 className="title1 text-title1">hello arman</h1>
+    </main>
   );
 }
