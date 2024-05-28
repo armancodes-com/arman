b24346e75caa32cc958a61c678ca72e1a9fbcb76
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-05-28 06:45:40 +0330

    configured tailwind colors and font sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main className="bg-red-500 text-center">hello arman</main>;
+  return (
+    <main className="bg-red-500 text-center dark:bg-blue-500">hello arman</main>
+  );
 }
