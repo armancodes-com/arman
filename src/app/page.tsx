d049ba72706df9539c3a0139b49dfacc83f8ac63
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-01 19:35:08 +0330

    feat: add hero section for desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+import { firaCode } from "./fonts";
+
 export default function Home() {
   return (
-    <main className="bg-red-500 text-center dark:bg-blue-500">
-      <h1 className="title1 text-title1">hello arman</h1>
+    <main className="pt-14.5">
+      <section className="flex h-[350px] gap-x-14">
+        <article className="flex flex-1 flex-col gap-y-4 py-10">
+          <div className="w-21 rounded-5 block h-1 min-h-1 bg-[#7127BA]"></div>
+
+          <div className={`text-title1 ${firaCode.className} font-bold`}>
+            <h1>Arman</h1>
+            <h2 className="text-3xl text-primary">Back-end Engineer</h2>
+          </div>
+
+          <p className="text-body1 text-text-primary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas
+            purus viverra accumsan Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Egestas purus viverra accumsan
+          </p>
+        </article>
+        <figure className="rounded-20 shadow-heroImage2 relative h-[422px] w-[384px] flex-1 rotate-6 overflow-hidden">
+          <Image
+            src={"/images/avatar.webp"}
+            alt="hero images"
+            fill
+            className="h-full w-full object-cover object-center"
+          />
+        </figure>
+      </section>
     </main>
   );
 }
