240c39832ba1681a3c0c6954474ed36e93268b21
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-02 07:05:02 +0330

    enhancement: create a new component for home page hero section

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import HomeHeroSection from "./_components/HomeHeroSection";
+
+export default function Home() {
+  return (
+    <main className="min-h-svh pt-4 sm:pt-14.5">
+      <HomeHeroSection />
+    </main>
+  );
+}
