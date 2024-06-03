11732d9bf7d6c0a97c7a414aa8c1d7713996a9e7
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-03 10:49:10 +0330

    update toolbar links component spacing

--- src/components/Navigation/ToolbarLinks.tsx
+++ src/components/Navigation/ToolbarLinks.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const ToolbarLinks = () => {
   return (
-    <aside className="fixed left-106 top-36 z-20 hidden flex-col gap-y-8 rounded-40 border border-primary px-3 py-2 lg:flex">
+    <aside className="left-112 xl:left-106 fixed top-36 z-20 hidden flex-col gap-y-8 rounded-40 border border-primary px-3 py-2 lg:left-36 lg:flex">
       <div className="group flex h-8 w-8 items-center justify-center">
         <Link href={"/"}>
           <GithubIcon className="transition-all duration-100 ease-in group-hover:[&_path]:fill-primary" />
