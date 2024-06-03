4e339a7fdf6fd017c0d091037afdd4f34823de76
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-03 10:00:03 +0330

    bug: resolve theme switcher bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <Analytics />
       <SpeedInsights />
       <body className={`${ubuntu.className} bg-bgColor`}>
