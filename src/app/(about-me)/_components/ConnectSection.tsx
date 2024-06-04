e723f3b1197fe57acc7f6f9dade504be92d328da
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-04 11:19:47 +0330

    created SectionHeader component for common sections' header

--- src/app/(about-me)/_components/ConnectSection.tsx
+++ src/app/(about-me)/_components/ConnectSection.tsx
@@ -1,24 +1,10 @@
-import { Fira_Code } from "next/font/google";
 import ConnectLinksList from "./ConnectList";
-
-const firaCode = Fira_Code({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  preload: true,
-});
-
+import SectionHeader from "@/components/ui/SectionHeader";
 
 const ConnectSection = () => {
   return (
     <section className="mt-11 space-y-4 px-4 md:mt-20 md:space-y-10 md:p-0">
-      <header>
-        <h2
-          className={`${firaCode.className} text-body1 font-bold capitalize md:text-title3`}
-        >
-          connect
-        </h2>
-      </header>
-
+      <SectionHeader title="connect" />
       <ConnectLinksList />
     </section>
   );
