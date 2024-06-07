fcad38503eb96d02fd8d5cf3881f5295a67652b9
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-07 10:40:45 +0330

    add single article page's header section

--- src/app/(articles)/articles/[slug]/page.tsx
+++ src/app/(articles)/articles/[slug]/page.tsx
@@ -1,9 +1,50 @@
+import IconHeart from "@/assets/icons/HeartIcon";
+import IconShare from "@/assets/icons/ShareIcon";
 import BackLink from "@/components/ui/BackLink";
 
 const Page = () => {
   return (
     <main className="min-h-svh px-4 pt-6 md:px-0 md:pt-11">
       <BackLink href="/articles">back</BackLink>
+
+      <header className="mb-11 mt-12 space-y-2 md:mb-12 md:mt-14 md:space-y-5">
+        <h2 className="text-body2 text-primary md:text-title2">
+          What does it take to become a web developer?
+        </h2>
+
+        <div className="flex items-center justify-between border-y border-border-articles py-2 md:py-4">
+          <div className="flex items-center gap-6">
+            <p className="space-x-2 text-text-primary">
+              <span className="text-xs font-bold tracking-wide md:text-caption2">
+                Date
+              </span>
+              <span className="text-xs font-light md:text-caption2">
+                10, Oct 2023
+              </span>
+            </p>
+            <p className="space-x-2 text-text-primary">
+              <span className="text-xs font-bold tracking-wide md:text-caption2">
+                Read
+              </span>
+              <span className="text-xs font-light md:text-caption2">1 Min</span>
+            </p>
+          </div>
+
+          <div className="flex items-center gap-9">
+            <IconShare
+              viewBox="0 0 32 32"
+              className="h-6 w-6 cursor-pointer md:h-8 md:w-8"
+            />
+            <IconHeart
+              viewBox="0 0 32 32"
+              className="h-6 w-6 cursor-pointer md:h-8 md:w-8"
+            />
+          </div>
+        </div>
+      </header>
+
+      {/* body section */}
+      <section className="border border-red-500"></section>
     </main>
   );
 };
---
+++ src/assets/icons/HeartIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const IconHeart = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={32}
+    height={32}
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M16.827 27.7467C16.3737 27.9067 15.627 27.9067 15.1737 27.7467C11.307 26.4267 2.66699 20.92 2.66699 11.5867C2.66699 7.46669 5.98699 4.13336 10.0803 4.13336C12.507 4.13336 14.6537 5.3067 16.0003 7.12003C17.347 5.3067 19.507 4.13336 21.9203 4.13336C26.0137 4.13336 29.3337 7.46669 29.3337 11.5867C29.3337 20.92 20.6937 26.4267 16.827 27.7467Z"
+      stroke="#282C33"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default IconHeart;
