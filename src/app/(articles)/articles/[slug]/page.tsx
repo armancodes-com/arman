d8d3d12eb70a1254ee6251bc1bd146a3165699c5
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-07 11:27:33 +0330

    add sample article paragraph test

--- src/app/(articles)/articles/[slug]/page.tsx
+++ src/app/(articles)/articles/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { Alexandria } from "next/font/google";
 
 const alexandria = Alexandria({
   subsets: ["latin"],
-  weight: ["400", "600", "700"],
+  weight: ["300", "400", "600", "700"],
   preload: true,
 });
 
@@ -80,7 +80,29 @@ const Page = () => {
           </div>
 
           {/* CONTENT OF ARTICLE */}
-          <div className="">content ...</div>
+          <div
+            className={`${alexandria.className} text-body2 font-light md:text-body1`}
+          >
+            <p className="text-inherit">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae
+              congue mauris rhoncus aenean vel elit scelerisque. In egestas erat
+              imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
+              senectus et netus. Mattis pellentesque id nibh tortor id aliquet
+              lectus proin. Sapien faucibus et molestie ac feugiat sed lectus
+              vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt
+              ornare massa eget. Dictum varius duis at consectetur lorem. Nisi
+              vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut
+              tortor pretium viverra suspendisse potenti nullam. Et molestie ac
+              feugiat sed lectus. Non nisi est sit amet facilisis magna.
+              Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut
+              enim blandit volutpat maecenas volutpat. Ornare lectus sit amet
+              est placerat in egestas erat. Nisi vitae suscipit tellus mauris a
+              diam maecenas sed. Placerat duis ultricies lacus sed turpis
+              tincidunt id aliquet.
+            </p>
+          </div>
         </div>
 
         {/* SIDEBAR OF SINGLE ARTICLES */}
