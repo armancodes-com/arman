61babc1c58421aff1bc08d92fe6a9b64ca2eef19
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-10 23:30:17 +0330

    update csp header

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
   // style-src 'self' 'nonce-${nonce}' 'unsafe-inline';
   const cspHeader = `
     default-src 'self'; 
-    script-src 'self' https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval'; 
+    script-src 'self' https://www.googletagmanager.com https://vercel.live https://va.vercel-scripts.com 'unsafe-inline' 'unsafe-eval'; 
     style-src 'self' 'unsafe-inline' 'unsafe-eval'; 
     img-src 'self' blob: data:; 
     font-src 'self'; 
