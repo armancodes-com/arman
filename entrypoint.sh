d03a176de6272467910f2f860d82ce5abf94dea1
Arman Ahmadi <22839699+armancodes@users.noreply.github.com>
2024-06-11 14:43:00 +0200

    tweak entrypoint.sh file (#93)
    * Docker deployment (#89)
    
    * Update Dockerfile
    
    * Update docker-compose.yml
    
    * Create deploy.yml
    
    * Update docker-compose.yml
    
    * Update deploy.yml
    
    * Update docker-compose.yml (#90)
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update Dockerfile
    
    * Update docker-compose.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Create .dockerignore
    
    * Update Dockerfile
    
    * Update docker-compose.yml
    
    * Update Dockerfile
    
    * Update next.config.mjs
    
    * Update docker-compose.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update docker-compose.yml
    
    * Create entrypoint.sh
    
    * Update docker-compose.yml
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check if a container with the same name already exists
-if [ "$(docker ps -aqf name=$CONTAINER_NAME)" ]; then
+if [ "$(sudo docker ps -aqf name=$CONTAINER_NAME)" ]; then
   # If a container with the same name exists, stop and remove it
-  docker stop ${COMPOSE_PROJECT_NAME}
-  docker rm ${COMPOSE_PROJECT_NAME}
+  sudo docker stop ${COMPOSE_PROJECT_NAME}
+  sudo docker rm ${COMPOSE_PROJECT_NAME}
 fi
 
 # Start your application
