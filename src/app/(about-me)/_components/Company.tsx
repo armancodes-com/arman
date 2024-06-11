add7ec9d6a2c40b594b7730bc3fd1356d7fd36ea
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-11 19:31:36 +0330

    enhancement: update company item design

--- src/app/(about-me)/_components/Company.tsx
+++ src/app/(about-me)/_components/Company.tsx
@@ -7,30 +7,35 @@ const Company: React.FC<ICompanyInfoProps> = ({
   image,
   location,
   role,
+  jobType,
   startDate,
 }) => {
   return (
-    <article className="group flex flex-wrap items-center justify-between transition-all duration-100 ease-in">
-      <div className="flex items-center gap-x-4 md:gap-x-6">
-        <figure className="relative h-[50px] w-[50px] overflow-hidden rounded-full border-primary transition-all duration-150 ease-linear group-hover:border-2 md:h-[80px] md:w-[80px]">
-          <Image
-            src={image}
-            alt="company name"
-            fill
-            className="h-full w-full object-cover object-center"
-          />
-        </figure>
+    <article className="group flex h-full items-center gap-x-6">
+      <figure className="relative h-[50px] w-[50px] overflow-hidden rounded-full border-primary transition-all duration-150 ease-linear group-hover:border-2 md:h-[80px] md:w-[80px]">
+        <Image
+          src={image}
+          alt="company name"
+          fill
+          className="h-full w-full object-cover object-center"
+        />
+      </figure>
 
+      <div className="flex flex-[6] flex-col justify-between gap-y-2 sm:flex-row">
         <p className="text-caption2 font-light md:text-caption1">
           <span className="group-hover:font-bold group-hover:text-primary md:font-medium">
             {role} at {companyName}
           </span>
-          <span className="text-text-secondary"> - {location}</span>
+          <span className="text-text-secondary">
+            {" "}
+            - {location} · {jobType}
+          </span>
         </p>
+
+        <span className="inline-flex w-full max-w-28 text-caption2 font-light text-text-primary group-hover:font-bold sm:m-0 sm:justify-end md:max-w-40 md:text-caption1 md:font-normal">
+          {startDate} - {endData}
+        </span>
       </div>
-      <span className="ml-[4rem] inline-flex w-full max-w-28 text-left text-caption2 font-light text-text-primary group-hover:font-bold sm:m-0 md:max-w-40 md:justify-end md:text-caption1 md:font-normal">
-        {startDate} - {endData}
-      </span>
     </article>
   );
 };
