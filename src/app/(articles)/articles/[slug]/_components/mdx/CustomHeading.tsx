95659db94ada75a140c925cf7681b0979e986f99
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-15 01:04:06 +0330

    Staging (#124)
    * update ellipse's height in largge screens
    
    * refactor articles to have ordeder
    
    * add multi env docker (#121)
    
    * add multi env docker
    
    * wip
    
    * refactor content file name order and extract numbers from creating their slug
    
    * add custom heading component t with click and copying it with title ids
    
    * refactor custom component to ho have id in order to make scroll happen
    
    * refactor formatting publishedAt date into a separate function
    
    ---------
    
    Co-authored-by: Arman Ahmadi <22839699+armancodes@users.noreply.github.com>

---
+++ src/app/(articles)/articles/[slug]/_components/mdx/CustomHeading.tsx
@@ -0,0 +1,155 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+"use client";
+
+import { usePathname } from "next/navigation";
+import { ReactNode, useState } from "react";
+import { Alexandria } from "next/font/google";
+
+const alexandria = Alexandria({
+  subsets: ["latin"],
+  weight: ["300"],
+  preload: true,
+});
+
+interface ICustomHeadingProps {
+  children: string | ReactNode;
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  id?: string;
+}
+
+const CustomHeadingWrapper: React.FC<{
+  tooltip: boolean;
+  children: ReactNode;
+}> = ({ children, tooltip }) => {
+  return (
+    <div className="relative">
+      {children}
+
+      {tooltip && (
+        <div
+          className={`absolute -left-[5.5rem] top-[120%] z-20 whitespace-nowrap rounded-5 bg-gray-2 p-2 text-caption2 font-light text-white ${alexandria.className}`}
+        >
+          copied to clipboard
+          <div className="absolute bottom-full left-1/2 -ml-1 rotate-180 !border-[5px] border-t border-transparent border-t-gray-2"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const CustomHeading: React.FC<ICustomHeadingProps> = ({
+  children,
+  as = "h1",
+  id,
+}) => {
+  const [tooltip, showTooltip] = useState(false);
+  const url = usePathname();
+
+  const headingClass = `relative before:invisible before:absolute before:-left-6 before:cursor-pointer before:text-gray-2 before:opacity-0 before:transition-all before:duration-200 before:ease-linear before:content-['#'] hover:before:visible hover:before:opacity-100`;
+
+  const baseUrlSection =
+    typeof window !== "undefined" &&
+    `${window.location.protocol}//${window.location.host}`;
+
+  const copyToClipboard = () => {
+    try {
+      navigator.clipboard.writeText(`${baseUrlSection}${url}#${id}`);
+      showTooltip(true);
+      setTimeout(() => showTooltip(false), 3000); // Tooltip disappears after 3 seconds
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+
+  if (as === "h1") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h1
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h1>
+      </CustomHeadingWrapper>
+    );
+  }
+
+  if (as === "h2") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h2
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h2>
+      </CustomHeadingWrapper>
+    );
+  }
+
+  if (as === "h3") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h3
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h3>
+      </CustomHeadingWrapper>
+    );
+  }
+
+  if (as === "h4") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h4
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h4>
+      </CustomHeadingWrapper>
+    );
+  }
+
+  if (as === "h5") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h5
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h5>
+      </CustomHeadingWrapper>
+    );
+  }
+
+  if (as === "h6") {
+    return (
+      <CustomHeadingWrapper tooltip={tooltip}>
+        <h6
+          className={headingClass}
+          onClick={copyToClipboard}
+          onKeyDown={copyToClipboard}
+          id={id}
+        >
+          {children}
+        </h6>
+      </CustomHeadingWrapper>
+    );
+  }
+};
+
+export default CustomHeading;
