38c30c5a583b35d9fd0923b7f6a47ef7720850f8
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-15 18:40:38 +0330

    optimise hero image

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 import heroImage from "@/public/images/hero-img.webp";
+import heroImage2 from "@/public/images/hero-img-2.jpg";
 import armanFirstImage from "@/public/images/arman-one.webp";
 import armanSecondImage from "@/public/images/arman-two.jpeg";
 import logoDark from "@/public/images/sample-logo.svg";
@@ -6,4 +7,11 @@ import logoWhite from "@/public/images/sample-logo-white.svg";
 
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-export { heroImage, logoDark, logoWhite, armanFirstImage, armanSecondImage };
+export {
+  heroImage,
+  logoDark,
+  logoWhite,
+  armanFirstImage,
+  armanSecondImage,
+  heroImage2,
+};
