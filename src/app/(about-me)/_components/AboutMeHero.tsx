61071a97d6909add04aff77c471b60ba9c4d3181
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-15 07:10:38 +0330

    refactored about me hero imageges design

--- src/app/(about-me)/_components/AboutMeHero.tsx
+++ src/app/(about-me)/_components/AboutMeHero.tsx
@@ -22,18 +22,19 @@ const AboutMeHeroSection = () => {
       </header>
 
       <div className="space-y-6 px-4 md:space-y-11 md:px-0">
-        <div className="mx-auto grid grid-cols-2 grid-rows-2 items-center justify-items-center sm:w-1/2 md:w-full md:grid-cols-4 md:grid-rows-1">
-          <figure className="relative left-10 top-6 block h-[165px] w-[150px] -rotate-[13.84deg] self-center overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:top-0 md:h-[275px] md:w-[250px] md:hover:left-8">
+        <div className="mx-auto mb-32 grid grid-cols-2 grid-rows-1 items-center justify-items-center sm:w-1/2 md:mb-16 md:w-full md:grid-cols-4">
+          <figure className="relative left-14 top-6 block h-[240px] w-[270px] -rotate-6 self-center overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:h-[240px] md:w-[320px] md:hover:left-8">
             <Image
               src={heroImage}
               alt="hero image"
               priority
               fill
+              decoding="async"
               quality={70}
-              className="h-full w-full object-cover object-center"
+              className="h-full w-full object-cover object-top"
             />
           </figure>
-          <figure className="relative -left-12 top-6 block h-[165px] w-[150px] rotate-[7.39deg] overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:left-0 md:top-0 md:h-[275px] md:w-[250px] md:hover:left-4">
+          <figure className="relative -left-4 top-1/3 block h-[255px] w-[190px] rotate-[7.39deg] select-none overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:left-14 md:top-8 md:h-[250px] md:w-[230px] md:hover:left-16">
             <Image
               src={armanFirstImage}
               alt="hero image"
@@ -43,7 +44,7 @@ const AboutMeHeroSection = () => {
               className="h-full w-full object-cover object-center"
             />
           </figure>
-          <figure className="relative -top-4 left-10 block h-[165px] w-[150px] rotate-[7.39deg] overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:-left-2 md:top-0 md:h-[275px] md:w-[250px] md:-rotate-[13.84deg] md:hover:-left-4">
+          <figure className="relative hidden -rotate-6 select-none overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:left-8 md:top-6 md:block md:h-[235px] md:w-[280px] md:hover:left-4">
             <Image
               src={armanSecondImage}
               alt="hero image"
@@ -53,7 +54,7 @@ const AboutMeHeroSection = () => {
               className="h-full w-full object-cover object-center"
             />
           </figure>
-          <figure className="relative -left-12 -top-4 block h-[165px] w-[150px] -rotate-[13.84deg] overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:-left-8 md:top-0 md:h-[275px] md:w-[250px] md:rotate-[7.39deg] md:hover:-left-4">
+          <figure className="relative hidden overflow-hidden rounded-20 transition-all duration-150 ease-in-out hover:z-10 md:-left-3 md:top-8 md:block md:h-[260px] md:w-[220px] md:rotate-[7.39deg] md:hover:-left-4">
             <Image
               src={heroImage}
               alt="hero image"
