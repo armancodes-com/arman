4b93d2272a320ac09799586c27386fea754f2ca3
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-16 21:03:43 +0330

    enhancement: update logo image

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,17 @@ import armanFirstImage from "@/public/images/arman-one.webp";
 import armanSecondImage from "@/public/images/arman-two.jpeg";
 import logoDark from "@/public/images/sample-logo.svg";
 import logoWhite from "@/public/images/sample-logo-white.svg";
+import WHITE_LOGO_SVG from "@/public/images/white-logo.svg";
+import DARK_LOGO_SVG from "@/public/images/dark-logo.svg";
 
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-export { heroImage, logoDark, logoWhite, armanFirstImage, armanSecondImage };
+export {
+  heroImage,
+  logoDark,
+  logoWhite,
+  armanFirstImage,
+  armanSecondImage,
+  DARK_LOGO_SVG,
+  WHITE_LOGO_SVG,
+};
