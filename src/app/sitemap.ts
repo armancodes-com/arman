a3fe7fdc3f5a058f032fc1212d500b21029ca2d3
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-17 06:22:15 +0330

    updated changes and sitemap keys (#165)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { MetadataRoute } from "next";
 import { allArticles, Article as ArticleType } from "contentlayer/generated";
 
@@ -7,29 +8,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
   );
 
   const articlesUrls = publishedArticles?.map((article) => ({
-    url: `${article?.baseUrl}articles/$${article?.slug}`,
+    url: `${article?.baseUrl}articles/${article?.slug}`,
     lastModified: new Date(article?.publishedAt),
+    priority: 0.6,
+    changeFrequency: "monthly",
   }));
 
   return [
     {
       url: "https://armancodes.com",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: "https://armancodes.com/about-me",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: "https://armancodes.com/articles",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.7,
     },
+    // @ts-ignore
     ...articlesUrls,
   ];
 }
