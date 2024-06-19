660761ee9d7d52fa6e274610b7d81a91229a014d
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-06-19 08:07:57 +0330

    update google analytics components

--- src/services/GoogleAnalytics.tsx
+++ src/services/GoogleAnalytics.tsx
@@ -1,23 +1,58 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+"use client";
+
+import { useEffect } from "react";
 import Script from "next/script";
+import { usePathname } from "next/navigation";
 
 interface IGoogleAnalyticsProps {
   nonce: string;
 }
 
 const GoogleAnalytics: React.FC<IGoogleAnalyticsProps> = ({ nonce }) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      // @ts-ignore
+      if (typeof window.gtag !== "undefined") {
+        // @ts-ignore
+        window?.gtag("config", "G-49WYNRYB8Y", {
+          page_path: url,
+        });
+      }
+    };
+
+    handleRouteChange(pathname);
+
+    // Listen for changes in the pathname and handle route change
+    return () => {
+      handleRouteChange(pathname);
+    };
+  }, [pathname]);
+
   return (
     <>
       <Script
-        async
-        nonce={nonce}
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
-      ></Script>
-      <Script nonce={nonce} id="google-analytics">
-        {` window.dataLayer = window.dataLayer || []; function
-          gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}')`}
-      </Script>
+        strategy="afterInteractive"
+        nonce={nonce}
+      />
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}, {
+                page_path: window.location.pathname,
+              });
+          `,
+        }}
+      />
     </>
   );
 };
