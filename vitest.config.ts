e7196e2e04f754f5ad6b222df46480527bd404a1
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-05 08:38:25 +0330

    add tests for scroll-into-view helper function

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     setupFiles: path.resolve(__dirname, "setup.ts"),
     environmentMatchGlobs: [["**/*.test.tsx", "jsdom"]],
     reporters: "verbose",
+    environment: "jsdom",
     coverage: {
       provider: "v8",
       thresholds: {
