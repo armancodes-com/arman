3a26e982c0ef41c54d336013db81c5c1be72da4b
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-14 06:40:03 +0330

    refactor newsletter component

--- src/components/ui/Newsletter.tsx
+++ src/components/ui/Newsletter.tsx
@@ -11,7 +11,10 @@ const Newsletter = () => {
   const [shouldShowNewsLetter, setShouldShowNewsLetter] =
     useState<boolean>(true);
   const [isSubscripting, setIsSubscripting] = useState<boolean>(false);
-  const [message, setMessage] = useState<string | null>(null);
+  const [result, setResult] = useState<{
+    status: "success" | "failure";
+    message: "";
+  } | null>(null);
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -21,7 +24,7 @@ const Newsletter = () => {
 
   const submitEmailToNewsletter = async (email: string) => {
     setIsSubscripting(true);
-    setMessage(null);
+    setResult(null);
 
     const response = await fetch("/api/newsletter-subscription", {
       method: "POST",
@@ -36,17 +39,17 @@ const Newsletter = () => {
 
     // subscription was successful
     if (response.status === 201) {
-      setMessage(data?.message);
+      setResult({ status: "success", message: data?.message });
       setShouldShowNewsLetter(false);
     }
 
     if (response?.status === 403) {
-      setMessage(data?.message);
+      setResult({ status: "failure", message: data?.message });
       setShouldShowNewsLetter(true);
     }
 
     if (response?.status === 400) {
-      setMessage(data?.message);
+      setResult({ status: "failure", message: data?.message });
       setShouldShowNewsLetter(true);
     }
 
@@ -63,59 +66,72 @@ const Newsletter = () => {
 
   if (isSubscripting) {
     return (
-      <section className="flex items-center justify-center px-4 md:p-0">
+      <section className="mt-10 flex items-center justify-center px-4 md:p-0">
         <p className="text-center text-primary">loading ...</p>
       </section>
     );
   }
 
-  if (!shouldShowNewsLetter && message && !isSubscripting) {
+  if (
+    !shouldShowNewsLetter &&
+    result?.status === "success" &&
+    !isSubscripting
+  ) {
     return (
-      <section className="flex items-center justify-center px-4 md:p-0">
-        <p className="text-center text-primary">{message}</p>
+      <section className="mt-10 flex items-center justify-center px-4 md:p-0">
+        <p className="text-center text-primary">{result?.message}</p>
       </section>
     );
   }
 
   if (isNewsLetterFeatureReleased && shouldShowNewsLetter && !isSubscripting) {
     return (
-      <section className="px-4 md:p-0" data-testid="newsletter-section">
-        <article className="mt-[72px] flex flex-col items-center justify-center gap-3 rounded-10 border border-[#7127BACC] bg-tertiary-bg px-6 pb-4 pt-6 text-center dark:border-none md:mt-25 md:gap-6">
-          <h3
-            className={`${alexandria.className} text-body2 font-semibold text-text-primary md:text-xl md:font-bold`}
-          >
-            Subscribe to my newsletter
-          </h3>
-          <p
-            className={`${alexandria.className} text-caption2 font-light text-text-primary md:${ubuntu.className} leading-6 md:text-caption1`}
-          >
-            Monthly personal reading and updates on topics like tech, design,
-            productivity, programming, and more!
-          </p>
+      <>
+        <section className="px-4 md:p-0" data-testid="newsletter-section">
+          <article className="mt-[72px] flex flex-col items-center justify-center gap-3 rounded-10 border border-[#7127BACC] bg-tertiary-bg px-6 pb-4 pt-6 text-center dark:border-none md:mt-25 md:gap-6">
+            <h3
+              className={`${alexandria.className} text-body2 font-semibold text-text-primary md:text-xl md:font-bold`}
+            >
+              Subscribe to my newsletter
+            </h3>
+            <p
+              className={`${alexandria.className} text-caption2 font-light text-text-primary md:${ubuntu.className} leading-6 md:text-caption1`}
+            >
+              Monthly personal reading and updates on topics like tech, design,
+              productivity, programming, and more!
+            </p>
 
-          <form
-            className="flex w-full flex-col gap-4 px-4 sm:flex-row md:px-13"
-            onSubmit={handleSubscribeNewsletter}
-          >
-            <div className="flex-[4]">
-              <Input
-                type="email"
-                placeholder="your email"
-                onChange={handleEmailChange}
-              />
-            </div>
-            <div className="flex-1">
-              <button className="flex w-full items-center justify-center rounded-80 border border-primary px-4 py-2 text-body2 font-medium text-primary">
-                Sign up
-              </button>
-            </div>
-          </form>
+            <form
+              className="flex w-full flex-col gap-4 px-4 sm:flex-row md:px-13"
+              onSubmit={handleSubscribeNewsletter}
+            >
+              <div className="flex-[4]">
+                <Input
+                  type="email"
+                  placeholder="your email"
+                  onChange={handleEmailChange}
+                  disabled={isSubscripting}
+                />
+              </div>
+              <div className="flex-1">
+                <button className="flex w-full items-center justify-center rounded-80 border border-primary px-4 py-2 text-body2 font-medium text-primary">
+                  Sign up
+                </button>
+              </div>
+            </form>
 
-          <span className="mt-3 text-caption2 font-light text-text-primary md:mb-7 md:mt-0">
-            Join the 110 other readers.
-          </span>
-        </article>
-      </section>
+            <span className="mt-3 text-caption2 font-light text-text-primary md:mb-7 md:mt-0">
+              Join the 110 other readers.
+            </span>
+          </article>
+        </section>
+
+        {result?.status === "failure" && (
+          <section className="mt-10 flex items-center justify-center px-4 md:p-0">
+            <p className="text-center text-primary">{result?.message}</p>
+          </section>
+        )}
+      </>
     );
   }
 
