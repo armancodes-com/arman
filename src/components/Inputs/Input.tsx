bbc1ed1d0857349ee9eee5a4ac59e056b5f131ba
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-14 20:11:58 +0330

    resolve input and button componenets' dark theme when disabled

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -24,7 +24,7 @@ const Input: React.FC<IInputProps> = ({
       <input
         type={type}
         className={twMerge(
-          "w-full rounded-80 border border-border-articles bg-transparent px-4 py-2 text-caption2 leading-7 text-text-primary outline-primary disabled:cursor-not-allowed disabled:border disabled:border-gray-500 disabled:bg-gray-100",
+          "w-full rounded-80 border border-border-articles bg-transparent px-4 py-2 text-caption2 leading-7 text-text-primary outline-primary disabled:cursor-not-allowed disabled:border disabled:border-gray-500 disabled:bg-gray-100 dark:disabled:border-gray-950 dark:disabled:bg-gray-600 dark:disabled:text-white",
           hasSearchIcon && "pl-12",
           className,
         )}
