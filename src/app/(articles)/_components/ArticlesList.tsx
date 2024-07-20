02ff5bda3a6bb18910be27b222eae9574bd1e04c
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-20 08:55:13 +0330

    resolve bug showind pagination when publisheArticles is less than SHO… (#256)
    * resolve bug showind pagination when publisheArticles is less than SHOW_PER_PAGE in production and pass total number of articles as a props
    
    * refacor ArticlesList component to render filtered articles passed to it

--- src/app/(articles)/_components/ArticlesList.tsx
+++ src/app/(articles)/_components/ArticlesList.tsx
@@ -3,9 +3,7 @@
 import type { Article as ArticleType } from "contentlayer/generated";
 
 import ArticleItem from "./ArticleItem";
-import { IS_PRODUCTION } from "@/constants";
 import readingTime from "@/utils/reading-time";
-import reverseArrayHandler from "@/utils/reverse-array";
 import useQueryString from "@/hooks/useQueryString";
 import { useEffect, useState } from "react";
 import { SHOW_PER_PAGE } from "@/constants/Pagination.constants";
@@ -21,19 +19,11 @@ const ArticlesList: React.FC<IArticlesListProps> = ({ articles }) => {
     pageFromUrl ? +pageFromUrl : 0,
   );
 
-  const publishedArticles = articles?.filter((article) => !article?.isDraft);
-  const allArticlesReversed = reverseArrayHandler(articles);
-  const publishedArticlesReversed = reverseArrayHandler(publishedArticles);
-
-  const displayedArticles = IS_PRODUCTION
-    ? publishedArticlesReversed
-    : allArticlesReversed;
-
   // Calculate the starting and ending index of the articles to display
   const startIdx = (currentPage - 1) * SHOW_PER_PAGE;
   const endIdx = startIdx + SHOW_PER_PAGE;
 
-  const paginatedArticles = displayedArticles.slice(startIdx, endIdx);
+  const paginatedArticles = articles.slice(startIdx, endIdx);
 
   useEffect(() => {
     setCurrentPage(pageFromUrl ? +pageFromUrl : 0);
