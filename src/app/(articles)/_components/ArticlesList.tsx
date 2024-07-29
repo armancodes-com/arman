632603d6e3b8c4d031ea38af336e9ba4533bb88b
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-29 17:52:57 +0330

    resolve pagination bug: the user cannot set pages a number more than the total pages the do exist (#262)

--- src/app/(articles)/_components/ArticlesList.tsx
+++ src/app/(articles)/_components/ArticlesList.tsx
@@ -7,14 +7,18 @@ import readingTime from "@/utils/reading-time";
 import useQueryString from "@/hooks/useQueryString";
 import { useEffect, useState } from "react";
 import { SHOW_PER_PAGE } from "@/constants/Pagination.constants";
+import getTotalArticlesPage from "@/utils/pagination-utils";
 
 interface IArticlesListProps {
   articles: ArticleType[];
 }
 
 const ArticlesList: React.FC<IArticlesListProps> = ({ articles }) => {
-  const { getQueryStringValue } = useQueryString();
+  const { getQueryStringValue, createQueryString } = useQueryString();
   const pageFromUrl = getQueryStringValue("page");
+
+  const totalPages = getTotalArticlesPage(articles?.length, SHOW_PER_PAGE);
+
   const [currentPage, setCurrentPage] = useState<number>(
     pageFromUrl ? +pageFromUrl : 1,
   );
@@ -26,8 +30,14 @@ const ArticlesList: React.FC<IArticlesListProps> = ({ articles }) => {
   const paginatedArticles = articles.slice(startIdx, endIdx);
 
   useEffect(() => {
-    setCurrentPage(pageFromUrl ? +pageFromUrl : 1);
-  }, [pageFromUrl]);
+    if (pageFromUrl) {
+      if (+pageFromUrl <= totalPages) {
+        setCurrentPage(pageFromUrl ? +pageFromUrl : 1);
+      } else {
+        createQueryString("page", `1`);
+      }
+    }
+  }, [pageFromUrl, totalPages, createQueryString]);
 
   return (
     <div className="mt-8 flex flex-col gap-8 md:mt-12 md:gap-10">
